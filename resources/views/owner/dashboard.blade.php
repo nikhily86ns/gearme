8b50110b56695d5b69f7d93fe845fe4dc68d4802
Nikhil <nikhily86.ns@gmail.com>
2021-11-08 14:03:54 +0530

    image issue fix

--- resources/views/owner/dashboard.blade.php
+++ resources/views/owner/dashboard.blade.php
@@ -476,6 +476,7 @@
 @endsection
 
 @section('extra-script')
+<script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 <script>  
 	$(function() {
 		// Multiple images preview with JavaScript
