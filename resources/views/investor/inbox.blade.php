7e9bb9c2f667d98a4a7917baee9242f8e57fb798
Nikhil <nikhily86.ns@gmail.com>
2021-11-22 12:35:14 +0530

    Proposal Post & Proposal View Done

--- resources/views/investor/inbox.blade.php
+++ resources/views/investor/inbox.blade.php
@@ -278,7 +278,7 @@
 								<tr>
 									<td class="name"><a href="#">Larry Gardner</a></td>
 									<td class="subject"><a href="#">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed </a></td>
-									<td class="time">08:30 PM</td>
+									<td class="time">08:30 PM</td>  
 								</tr>
 							</tbody></table>
 						</div>
